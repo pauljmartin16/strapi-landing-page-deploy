01508a90a7e185651c19059097843332c192b2bb
pauljmartin16 <pauljmartin16@gmail.com>
2024-07-17 08:02:15 -0400

    initial commit

---
+++ src/api/landing-page/middlewares/landing-page-populate.js
@@ -0,0 +1,54 @@
+'use strict';
+
+
+const populate = {
+    metadata: {
+      populate: {
+        metaImage: {
+          populate: true,
+          fields: ["name", "alternativeText", "url"]
+        }
+      }
+    },
+    blocks: {
+      populate: {
+        link: {
+          populate: true
+        },
+        image: {
+          fields: ["name", "alternativeText", "url"]
+        },
+        card: {
+          populate: {
+            image: {
+              fields: ["name", "alternativeText", "url"]
+            }
+          }
+        },
+        plan: {
+          populate: ["services", "link"]
+        },
+        form: {
+          populate: ["input", "button"]
+        }
+      }
+    }
+  };
+
+
+/**
+ * `landing-page-populate` middleware
+ */
+
+module.exports = (config, { strapi }) => {
+  // Add your own logic here.
+  return async (ctx, next) => {
+    strapi.log.info('In landing-page-populate middleware.');
+    ctx.query = {
+      populate,
+      ...ctx.query
+    }
+
+    await next();
+  };
+};
---
+++ src/api/landing-page/routes/landing-page.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * landing-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+// module.exports = createCoreRouter('api::landing-page.landing-page');
+module.exports = createCoreRouter('api::landing-page.landing-page', {
+    config: {
+        find: {
+            middlewares: ['api::landing-page.landing-page-populate']
+        },
+        findOne: {
+            middlewares: ['api::landing-page.landing-page-populate']
+        }
+    }
+});
